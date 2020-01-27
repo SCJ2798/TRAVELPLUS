0d66766bf9600593163820da7e1ff3832545ab16
Chathuranga Jayawardana <36012203+SCJ2798@users.noreply.github.com>
2020-01-28 01:12:09 +0530

    Add files via upload

---
+++ public/js/style.js
@@ -0,0 +1,14 @@
+if(screen.width > 1024){
+var sh = document.getElementsByClassName("s_he");
+var imgSlider_width = document.getElementById("carouselExampleIndicators").offsetHeight;
+sh[0].style.height = imgSlider_width+"px";
+
+//sh[0].style.height = screen.height+"px";
+sh[1].style.height = imgSlider_width+"px";
+//sh[2].style.height = screen.height+"px";
+sh[3].style.height = imgSlider_width+"px";
+
+//console.log(s_c.offsetHeight);
+
+}
+
